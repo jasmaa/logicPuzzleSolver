c85a6a35f203b675b1f0abdd24b66d4401c5edff
jasmaa <jasonmaa3955@gmail.com>
2017-12-11 18:40:53 -0500

    Add poe

--- prototyping/test_logic.py
+++ prototyping/test_logic.py
@@ -36,6 +36,8 @@ constraints = [
         ['WALLACE', 'SL'],
     ]
 
+constraint_graveyard = []
+
 """
 
 PERSON, PLACE, YEAR
@@ -68,19 +70,42 @@ CONSTRAINTS
 
 -disassociations (harry does not own the broom)
 -POE (harry owns the broom so no one else can own the broom)
--links ()
+-links (harry is definitely associated with broom,
+        broom is not associated with 42 or 39,
+        therefore, harry is not associated with 42 or 39)
 
 """
 
 # go until no constraints left to apply
-for c in constraints:
-    for p in possible:
+while len(constraints) > 0:
 
-        # diconnect c1
+    c = constraints[0]
+
+    # loop for disassociations
+    for p in possible:
+        # disconnect c1
         if c[0] in p and c[1] in p[c[0]]:
             p[c[0]].pop(p[c[0]].index(c[1]))
         # diconnect c0
         elif c[1] in p and c[0] in p[c[1]]:
             p[c[1]].pop(p[c[1]].index(c[0]))
+
+    # check for POE
+    for p in possible:
+        for feature in p:
+            if len(p[feature]) == 1:
+                poe_list = list(p.keys())
+                poe_list.pop(poe_list.index(feature))
+                
+                for f in poe_list:
+                    new_c = [f, p[feature][0]]
+                    if new_c not in constraint_graveyard:
+                        constraints.append(new_c)
+                        constraint_graveyard.append(new_c)
+
+    # TODO implement links
+                        
+    #pp.pprint(possible)
+    constraint_graveyard.append(constraints.pop(0))
 
 pp.pprint(possible)
